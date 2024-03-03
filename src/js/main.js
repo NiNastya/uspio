a7e9bc11d1dc3a80bc35c214be1d9a172e9e153c
Real Re <realre1337@gmail.com>
2024-03-03 20:51:38 +0600

    Add Logicks

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,44 @@
 import '../scss/style.scss'
+import axios from 'axios';
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    slidesPerView: 3,
+    loop: true,
+    lazy: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        960: {
+            slidesPerView: 3,
+        },
+        320: {
+            slidesPerView: 1,
+        },
+    }
+});
 
+// отправка формы 
+
+document.getElementById('subscribe').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const formData = new FormData(this);
+    axios.post('/public/send.json', formData)
+        .then(response => {
+            console.log(response.data);
+            document.getElementById('btn-form').innerText = response.data.message
+            document.getElementById('btn-form').style.background = "#000";
+        })
+        .catch(error => {
+            console.error('Error', error);
+        });
+});
+
+// Устанавливаем изображение в модальное окно 
+var images = document.getElementsByClassName("ModalImg");
+for (var i = 0; i < images.length; i++) {
+    images[i].addEventListener("click", function (event) {
+        document.querySelector('.modal-img').src = event.target.currentSrc
+    });
+}
